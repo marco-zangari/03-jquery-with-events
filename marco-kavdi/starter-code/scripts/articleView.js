d273690baf749d616282d70d8e8a1b7ff2059b0d
Marco Zangari <mjzangari@gmail.com>
2017-09-14 10:18:26 -0700

    working on third TODO, option pull down

--- marco-kavdi/starter-code/scripts/articleView.js
+++ marco-kavdi/starter-code/scripts/articleView.js
@@ -38,6 +38,10 @@ articleView.handleAuthorFilter = function() {
     //         onto it.
     if ($(this).val()) {
       // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
+      $('option').on('click', function(){
+        $('article').hide();
+      })
+
       //       and then show just the ones that match for the author that was selected.
       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
 
