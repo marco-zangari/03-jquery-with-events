1e51b4e82e069a499ed326cb2a044059b51d982c
Marco Zangari <mjzangari@gmail.com>
2017-09-14 10:54:28 -0700

    filtered for the article

--- marco-kavdi/starter-code/scripts/articleView.js
+++ marco-kavdi/starter-code/scripts/articleView.js
@@ -39,13 +39,13 @@ articleView.handleAuthorFilter = function() {
     if ($(this).val()) {
 
     // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
-      $('articles').hide();
+      $('article').hide();
       $(this).val().appendTo('body');
       //       and then show just the ones that match for the author that was selected.
       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
 
     } else {
-      $('articles').show();
+      $('article').show();
       // TODO: If the select box was changed to an option that is blank, we should
       //       show all the articles, except the one article we are using as a template.
 
